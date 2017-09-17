b5035740330ed1983251fa16969320d3a9de9259
AlanaGonzagaCunha <alanagonzagac@gmail.com>
2017-09-17 19:39:25 -0300

    chat

--- Redes2/src/br/unifor/redes2/cliente/Cliente.java
+++ Redes2/src/br/unifor/redes2/cliente/Cliente.java
@@ -6,7 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 import javax.swing.JTextArea;
@@ -16,83 +18,48 @@ public class Cliente {
 
 	private static int port = 8080;
 	private static String host = "localhost";
-	private static String name;
-
-	public Cliente(String host, int port, String name) {
-		this.port = port;
-		this.host = host;
-		this.name = name;
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void recebeMgs(Object append) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public static void run() throws IOException {
-		System.out.println("Iniciando cliente...");
-
-			System.out.println("Iniciando conexão com o servidor...");
-			Socket socket = new Socket(host, port);
-			System.out.println("Conexão cliente estabelecida como servidor ");
-
-			InputStream input = socket.getInputStream();
-			OutputStream output = socket.getOutputStream();
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(input));
-			PrintStream out = new PrintStream(output);
-
-			Scanner entrada = new Scanner(System.in);
-			
-			while (true) {
-				System.out.println("Digite mgs;");
-				String m = entrada.nextLine();
-
-				out.println(m);
-				m = in.readLine();
-				System.out.println("Messagem recebida: " + m);
-
-			}
-		
-		System.out.println("Encerrando conexões...");
-		in.close();
-		out.close();
-		socket.close();
-
-		}
-
-	
 
 	public static void main(String[] args) {
+
 		try {
-			run();
+			Socket cliente = new Socket(host, port);
+
+			new Thread() {
+				public void run() {
+
+					try {
+						BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+						while (true) {
+							try {
+								String mgs = in.readLine();
+								if(mgs == null || mgs.isEmpty())
+									continue;
+								System.out.println("Servidor: " + mgs);
+
+							} catch (IOException e) {
+								e.printStackTrace();
+								System.out.println("Impossível receber messagens do servidor... ");
+								
+							}
+
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				};
+
+			}.start();
+
 			
+
+		} catch (UnknownHostException e) {
+			System.out.println("Endereço inválido! ");
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Não é possível conectar com o servidor. ");
 		}
 	}
 
--- Redes2/src/br/unifor/redes2/swing/Principal.java
+++ Redes2/src/br/unifor/redes2/swing/Principal.java
@@ -1,23 +1,36 @@
 package br.unifor.redes2.swing;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Iterator;
 
+import javax.swing.AbstractButton;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import br.unifor.redes2.cliente.Cliente;
-import br.unifor.redes2.sever.Servidor;
 
-public class Principal {
+public class Principal extends JFrame {
 	static Cliente cliente;
+	private JList exibeUsuarios;
 	private JFrame janela;
 	private JButton btnEnviar;
 	private JPanel painelChat;
@@ -25,75 +38,184 @@ public class Principal {
 	private JScrollPane scrollTxt;
 	private JTextArea exibeMgs;
 	private Socket socket;
-	private static int port=8080;
-	private static String host="localhost";
+	private static int port = 8080;
+	private static String host = "localhost";
+	private PrintWriter out;
 
 	public Principal(Cliente cliente) {
 		montaJanela();
 	}
 
-	public void montaJanela( ) {
+	public void montaJanela() {
 		janela = new JFrame();
 		btnEnviar = new JButton("Enviar");
 		painelChat = new JPanel();
 		txtArea = new JTextArea();
 		exibeMgs = new JTextArea();
+		exibeUsuarios = new JList();
 		scrollTxt = new JScrollPane(txtArea);
-				
-		janela.setTitle("Chat Redes 2");
-		janela.setVisible(true);
-		janela.setSize(600, 500);
-		janela.setLocation(200, 100);
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		janela.setLayout(null);
-	
 		
+
+		setTitle("Chat Redes 2");
+		setVisible(true);
+		setSize(600, 500);
+		setLocation(200, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);	
+
 		btnEnviar.setSize(100, 110);
 		btnEnviar.setLocation(480, 345);
 
 		txtArea.setLineWrap(true);
 		txtArea.setWrapStyleWord(true);
-		txtArea.setSize(500, 110);
-		txtArea.setLocation(5, 345);
-		
-		exibeMgs.setSize(570,330);
-		exibeMgs.setLocation(5,10);
+		txtArea.setSize(370, 110);
+		txtArea.setLocation(105, 345);
+
+		exibeMgs.setSize(470, 330);
+		exibeMgs.setLocation(105, 10);
 		exibeMgs.setEditable(false);
+			
+		exibeUsuarios.setBackground(Color.gray);
+		exibeUsuarios.setSize(100, 450);
+		exibeUsuarios.setLocation(0, 10);
+
+		scrollTxt.setBounds(105, 10, 470, 290);
+		scrollTxt.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollTxt.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollTxt.setViewportView(exibeMgs);
+		repaint();
+
+		add(exibeUsuarios);
+		add(btnEnviar);
+		add(txtArea);
+		getContentPane().add(scrollTxt);
+
+		exibeMgs.setText("Chat iniciado... Porta: " + port + ", " + " Host: " + host + "\n");
+
+		repaint();
+		tratarEventos();
+		String [] users = {"Alana", "Maycom", "Milla", "Nina", "Valks"}; 
+		prencherListaDeUsuarios(users);
+	}
+
+	private void prencherListaDeUsuarios(String [] usuarios) {
+
+		DefaultListModel model = new DefaultListModel<>();
+		exibeUsuarios.setModel(model);
 		
-		
-		janela.add(btnEnviar);
-		janela.add(txtArea);	
-		janela.add(exibeMgs);
-		janela.add(scrollTxt);
-		
-		exibeMgs.setText("Chat iniciado... Porta: "+ cliente.getPort() +", " +" Host: "+ cliente.getHost() +"\n");	
-		
-		janela.repaint();
+		for (String user: usuarios) {
+			model.addElement(user);
+		}
+	}
+
+	private void tratarEventos() {
+
+		txtArea.addKeyListener(new KeyListener() {
+
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+					if (!txtArea.getText().isEmpty()) {
+						exibeMgs.append(txtArea.getText());
+						
+						out = new PrintWriter(cliente.getOutputStream(), true);
+						BufferedReader terminal = new BufferedReader(new InputStreamReader(System.in));
+
+						while (true) {
+
+							String mgsTerminal = terminal.readLine();
+							if(mgsTerminal== null || mgsTerminal.length() ==0) {
+								continue;
+							}
+							out.println(mgsTerminal);
+							if (mgsTerminal.equalsIgnoreCase("::SAIR")) {
+								System.exit(0);
+
+							} 
+						}
+						
+						txtArea.setText("");
+					}
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+			}
+		});
+
 		btnEnviar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-						
 
-					if (!txtArea.getText().isEmpty() && !txtArea.isFocusOwner()) {
-						//recebeMgs(exibeMgs.append(cliente.getName() + ": " + txtArea.getText()+"\n"));
-						txtArea.setText("");
-					}
-								
+				if (!txtArea.getText().isEmpty() && !txtArea.isFocusOwner()) {
+					exibeMgs.append(txtArea.getText() + "\n");
+					txtArea.setText("");
+				}
+
 			}
 
-			
 		});
 	}
 
-	public static void main(String[] args) {
-		String nameCliente= JOptionPane.showInputDialog("Entre com seu nome: ");
-		if(!nameCliente.isEmpty()) {
-			cliente  = new Cliente(host, port, nameCliente);
-			Servidor  server = new Servidor(port);
-			Principal p = new Principal(cliente);
+	public static void iniciarChat() {
+
+		try {
+			Socket cliente = new Socket(host, port);
+
+			new Thread() {
+				public void run() {
+
+					try {
+						BufferedReader in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+						while (true) {
+							try {
+								String mgs = in.readLine();
+								if(mgs == null || mgs.isEmpty())
+									continue;
+								System.out.println("Servidor: " + mgs);
+
+							} catch (IOException e) {
+								e.printStackTrace();
+								System.out.println("Impossível receber messagens do servidor... ");
+								
+							}
+
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				};
+
+			}.start();
+
 			
-		}else {
-		JOptionPane.showMessageDialog(null, "Por favor digite o nome...");
+
+		} catch (UnknownHostException e) {
+			System.out.println("Endereço inválido! ");
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Não é possível conectar com o servidor. ");
+		}
+	}
+	public static void main(String[] args) {
+		String nameCliente = JOptionPane.showInputDialog("Entre com seu nome: ");
+		if (!nameCliente.isEmpty()) {
+
+			Principal p = new Principal(cliente);
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Por favor digite o nome...");
 		}
 
 	}
